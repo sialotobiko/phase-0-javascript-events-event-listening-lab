ad0b5a5ffa90844611edf9d5729379828d2ed2b3
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-25 00:25:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let input;
+input = document.getElementById('input');
+
 function addingEventListener() {
+
+const input = document.getElementById('input');
+
+function clickAlert(){
+    alert('I am a label');
+}
+
+input.addEventListener('click', clickAlert);
 }
